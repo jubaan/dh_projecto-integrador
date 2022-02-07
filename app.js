d7b9cda0e53d2aede0f253da116cfa1f521c14b8
Julio Añoveros <julio.ab@outlook.com>
2022-02-06 18:14:49 -0600

    Add sequelize, mysql, methodOverride config

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var methodOverride = require('method-override');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+// methodOverride
+app.use(methodOverride('_method'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
